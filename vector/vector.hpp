3f4630c66aa772adcb4fdd1dbd27391ce9225068
hor4tio <hor4tio.code@gmail.com>
2021-12-03 00:17:24 +0100

    vector in the works

---
+++ vector/vector.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: allanganoun <allanganoun@student.42lyon    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 17:48:15 by allanganoun       #+#    #+#             */
+/*   Updated: 2021/12/03 00:14:59 by allanganoun      ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft
+{
+	template <typename T>
+	class vector
+	{
+	private:
+		/* data */
+	public:
+		vector(/* args */);
+		~vector();
+	};
+
+	vector::vector(/* args */)
+	{
+	}
+
+	vector::~vector()
+	{
+	}
+
+}
+
+#endif
