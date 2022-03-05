54d70f71f96b01824eebd866c5f32bf7ad6293d8
tglory <tglory@student.42Lyon.fr>
2022-03-05 22:38:16 +0100

    add map.hpp to test it

---
+++ srcs/utils/pair.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+namespace ft {
+template <class T1, class T2>
+struct pair {
+	typedef T1 first_type;
+	typedef T2 second_type;
+
+	T1 first;
+	T2 second;
+
+	pair() : first(), second() {
+	}
+
+	~pair() {
+	}
+
+	pair(const T1 &x, const T2 &y) : first(x), second(y) {
+	}
+
+	template<class U1, class U2>
+	pair(const pair<U1, U2> &p) : first(p.first), second(p.second) {
+	}
+
+	pair& operator=(const pair &other) {
+		first = other.first;
+		second = other.second;
+		return *this;
+	}
+};
+
+template<class T1, class T2>
+pair<T1, T2> make_pair(T1 x, T2 y) {
+	return ft::pair<T1, T2>(x, y);
+}
+
+template <class T1, class T2>
+bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+	return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+template <class T1, class T2>
+bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+	return !(lhs == rhs);
+}
+
+template <class T1, class T2>
+bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+	return lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second);
+}
+
+template <class T1, class T2>
+bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+	return !(rhs < lhs);
+}
+
+template <class T1, class T2>
+bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+	return rhs < lhs;
+}
+
+template <class T1, class T2>
+bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+	return !(lhs < rhs);
+}
+}
