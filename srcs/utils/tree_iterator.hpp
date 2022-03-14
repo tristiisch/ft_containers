09a5c05661e7b376f78f03e9b4dfca69c9a23304
hor4tio <hor4tio.code@gmail.com>
2022-03-14 17:52:19 +0100

    my tree added

--- srcs/utils/tree_iterator.hpp
+++ srcs/utils/tree_iterator.hpp
@@ -1,22 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tree.hpp                                           :+:      :+:    :+:   */
+/*   tree_iterator.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: allanganoun <allanganoun@student.42lyon    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/14 15:36:17 by allanganoun       #+#    #+#             */
-/*   Updated: 2022/03/14 17:49:45 by allanganoun      ###   ########lyon.fr   */
+/*   Created: 2022/03/14 15:42:23 by allanganoun       #+#    #+#             */
+/*   Updated: 2022/03/14 17:48:24 by allanganoun      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+# include "pair.hpp"
+
+template <class T1, class T2>
+struct	node
+{
+
+	ft::pair	p(T1 key, T2 value);
+	node 		*parent;
+	node(T1 key, T2 value) : p(key, value){}
+};
 
 namespace ft
 {
-	class tree
+	class tree_iterator
 	{
 	public :
 	private :
-	}
+		node *n();
+	};
 }
